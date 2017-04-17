276b08719d2b30e7effdc27e0a5ef6d64b3b340d
mknarciso <murilokn@gmail.com>
2017-04-17 02:37:49 +0000

    Tutorial do Maven até a etapa 6

---
+++ src/main/java/br/ita/App.java
@@ -0,0 +1,12 @@
+package br.ita;
+
+import com.google.gson.Gson;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Gson gson = new Gson();
+        System.out.println( "Hello World!" );
+    }
+}
